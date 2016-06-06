40edad46aaba1f4981fd2d3276203ce8f33f96b0
Jennifer Francis <jen.greatrix@gmail.com>
2016-06-06 15:15:27 -0400

    adding formating when selecting parent subject to make it easier to see parents, fixing update guardian bug

--- api/public/index.php
+++ api/public/index.php
@@ -6608,7 +6608,7 @@ $app->post('/addGuardian', function () use($app) {
 		 $db = null;
 
 		// if login data was passed
-		if( $login !== null )
+		if( $login !== null && isset($data['login']['username']) )
 		{
 			$allPostVars['guardian']['student_id'] = $studentId;
 			$allPostVars['guardian']['guardian_id'] = $guardian_id;
@@ -6711,7 +6711,7 @@ $app->put('/updateGuardian', function () use($app) {
 		$db = null;
 		
 		// if login data was passed
-		if( $login !== null )
+		if( $login !== null && isset($data['login']['username']) )
 		{
 			$allPostVars['guardian']['student_id'] = $studentId;
 			createParentLogin($allPostVars['guardian']);
