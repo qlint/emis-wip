25614fe37b24468037bdd540d7f3adbc00c8ce9e
Jennifer Francis <jen.greatrix@gmail.com>
2016-06-21 12:47:38 -0400

    updating db update

--- etc/db-clients-update-06212016.sql
+++ etc/db-clients-update-06212016.sql
@@ -2,4 +2,6 @@
 
 CREATE UNIQUE INDEX "U_active_class_name" ON app.classes (class_name, class_cat_id) WHERE active is true;
 
+ALTER TABLE app.class_cats DROP CONSTRAINT "U_active_class_cat";
 
+CREATE UNIQUE INDEX "U_active_class_cat" ON app.class_cats (class_cat_name) WHERE active is true;
