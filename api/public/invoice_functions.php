5b93d351ee6f40775eb7c2c250b68da02ebb3a83
Jennifer Francis <jen.greatrix@gmail.com>
2016-07-25 14:56:27 -0400

    reverting invoicing query

--- api/public/invoice_functions.php
+++ api/public/invoice_functions.php
@@ -78,12 +78,12 @@ $app->get('/generateInvoices/:term(/:studentId)', function ($term, $studentId = 
 							CASE
 								 WHEN payment_method = 'Installments' THEN
 								case when num_payments_this_term > 1 THEN
-									generate_series(date_last_invoice, date_last_invoice + ((payment_interval*(num_payments_this_term-1)) || payment_interval2)::interval, (payment_interval::text || payment_interval2)::interval)::date
+									generate_series(term_start_date, term_start_date + ((payment_interval*(num_payments_this_term-1)) || payment_interval2)::interval, (payment_interval::text || payment_interval2)::interval)::date
 								else
 									term_start_date
 								end 
 								 ELSE
-								term_start_date								
+								year_start_date								
 							END as due_date,
 							
 							coalesce(round((select sum(amount)  
@@ -122,6 +122,7 @@ $app->get('/generateInvoices/:term(/:studentId)', function ($term, $studentId = 
 														WHERE student_id = q.student_id
 													) q
 												   )q2
+												   WHERE due_date BETWEEN term_start_date and date_trunc('month',term_end_date) 
 												)
 
 											 ELSE 0 END
@@ -129,17 +130,16 @@ $app->get('/generateInvoices/:term(/:studentId)', function ($term, $studentId = 
 											-- are there any installments due this term
 											(SELECT count(*) FROM (
 												SELECT
-												generate_series(date_last_invoice, date_last_invoice + ((payment_interval*(num_per_pay_period-1)) || payment_interval2)::interval, (payment_interval::text || payment_interval2)::interval)::date
-												--generate_series(year_start_date, year_start_date + ((payment_interval*(num_payments-1)) || payment_interval2)::interval, (payment_interval::text || payment_interval2)::interval)::date  as due_date
+												generate_series(year_start_date, year_start_date + ((payment_interval*(num_payments-1)) || payment_interval2)::interval, (payment_interval::text || payment_interval2)::interval)::date  as due_date
 												FROM (
-														SELECT payment_interval,payment_interval2,num_payments,
-														coalesce( (select max(due_date) from app.invoices where invoices.student_id = students.student_id), (select start_date from app.current_term)) as date_last_invoice
+														SELECT payment_interval,payment_interval2,num_payments
 														FROM app.students
 														INNER JOIN app.installment_options 
 														ON installment_option_id = installment_options.installment_id
-														WHERE student_id = 5
+														WHERE student_id = q.student_id
 													) q
 												)q2
+												WHERE due_date BETWEEN term_start_date and date_trunc('month',term_end_date) 
 											)
 										END
 									 ELSE
@@ -151,8 +151,7 @@ $app->get('/generateInvoices/:term(/:studentId)', function ($term, $studentId = 
 											WHERE canceled = false
 											AND student_fee_item_id = q.student_fee_item_id
 										) = 0 THEN 1 ELSE 0 END
-								END::integer AS num_payments_this_term,
-								date_last_invoice
+								END::integer AS num_payments_this_term
 							FROM (
 								SELECT 
 									students.student_id, first_name, middle_name, last_name, 
@@ -162,11 +161,7 @@ $app->get('/generateInvoices/:term(/:studentId)', function ($term, $studentId = 
 									(select start_date from $termStatement) as term_start_date,
 									(select end_date from $termStatement) as term_end_date,
 									coalesce((select start_date from $nextTermStatement), (select end_date from $termStatement)) as start_next_term,
-									( select min(start_date) from app.terms where date_part('year',start_date) = date_part('year', (select start_date from $termStatement)) ) as year_start_date,
-									coalesce( (select max(due_date) from app.invoices where invoices.student_id = students.student_id), (select start_date from $termStatement)) as date_last_invoice,
-									case when payment_plan_name = 'Per Month' then 4
-										 when payment_plan_name = 'Per Term' then 1
-									end as num_per_pay_period
+									( select min(start_date) from app.terms where date_part('year',start_date) = date_part('year', (select start_date from $termStatement)) ) as year_start_date
 								FROM app.students									
 								INNER JOIN app.student_fee_items
 									INNER JOIN app.fee_items
