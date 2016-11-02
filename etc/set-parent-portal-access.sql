aa30f661350822f4dfd24cc091b2f6a0239dee10
Jennifer Francis <jen.greatrix@gmail.com>
2016-11-02 10:26:35 -0400

    adding sql script to set parent portal setting

---
+++ etc/set-parent-portal-access.sql
@@ -0,0 +1,8 @@
+﻿-- turn on parent portal for the first time
+INSERT INTO app.settings(name,value) values('Parent Portal', 'Yes');
+
+-- turn off parent portal
+-- UPDATE app.settings SET value = 'No' WHERE name = 'Parent Portal';
+
+-- turn back on after inactiving parent portal
+-- UPDATE app.settings SET value = 'Yes' WHERE name = 'Parent Portal';
