385cc0389967b677fefa36722d834636654648fe
andy15la <andrew_urban@ymail.com>
2018-05-02 13:31:00 +0300

    new analysis

--- src/overviews/kingsinternational-performance-analysis.php
+++ src/overviews/kingsinternational-performance-analysis.php
@@ -27,7 +27,6 @@
     <div class="container">
       <a class="navbar-brand" href="#"><?php echo htmlspecialchars( array_shift((explode('.', $_SERVER['HTTP_HOST']))) ); ?>.eduweb.co.ke Mean (x&#772;) Analysis</a>
       <a class="navbar-brand"></a>
-      <a class="navbar-brand" href="<?php echo htmlspecialchars("http://".array_shift((explode('.', $_SERVER['HTTP_HOST']))).".eduweb.co.ke/overviews"); ?>" style="color:#ffcc00;">Go Back</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -35,6 +34,11 @@
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
             <a class="nav-link" href="<?php echo htmlspecialchars("http://".array_shift((explode('.', $_SERVER['HTTP_HOST']))).".eduweb.co.ke"); ?>">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="<?php echo htmlspecialchars("http://".array_shift((explode('.', $_SERVER['HTTP_HOST']))).".eduweb.co.ke/overviews"); ?>" style="color:#0cff05;">Go Back
               <span class="sr-only">(current)</span>
             </a>
           </li>
