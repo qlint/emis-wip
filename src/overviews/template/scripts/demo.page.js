39724e1264c85d265ed03c68f6446f1fc9fb8d15
andy15la <andrew_urban@ymail.com>
2018-05-04 15:33:18 +0300

    custom naming and export on click

--- src/overviews/template/scripts/demo.page.js
+++ src/overviews/template/scripts/demo.page.js
@@ -1,21 +1,49 @@
 ﻿$(document).ready(function () {
     //mean analysis
-    excel = new ExcelGen({
-        "src_id": "test_table",
-        "show_header": true
-    });
+
     $("#generate-excel").click(function () {
+      excel = new ExcelGen({
+          "src_id": "test_table",
+          "show_header": true
+      });
+
         excel.generate();
     });
     //mean analysis sent_date
 
     //grade comparison
-    excel = new ExcelGen({
-        "src_id": "test_table2",
-        "show_header": true
-    });
+
     $("#generate-excel2").click(function () {
+      excel = new ExcelGen({
+          "src_id": "test_table2",
+          "show_header": true
+      });
+
         excel.generate();
     });
-    //grade comparison end
+    //grade comparison end.
+
+    //deviations
+
+    $("#generate-excel3").click(function () {
+      excel = new ExcelGen({
+          "src_id": "test_table3",
+          "show_header": true
+      });
+
+        excel.generate();
+    });
+    //deviations end.
+
+    //marks count
+
+    $("#generate-excel4").click(function () {
+      excel = new ExcelGen({
+          "src_id": "test_table4",
+          "show_header": true
+      });
+
+        excel.generate();
+    });
+    //marks count end.
 });
