fee176ceb511e7af008f1f28f82d0232087d1c4b
andy15la <andrew_urban@ymail.com>
2018-06-05 11:06:54 +0300

    blocking teacher comms

--- src/app/communications/listHomeworkCtrl.js
+++ src/app/communications/listHomeworkCtrl.js
@@ -3,27 +3,32 @@
 angular.module('eduwebApp').
 controller('listHomeworkCtrl', ['$scope', '$rootScope', 'apiService','$timeout','$window','$filter','$state',
 function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
-	
+
 	var initialLoad = true;
 	$scope.filters = {};
 	$scope.filters.post_status_id = null;
 	$scope.filters.class_id = ( $state.params.class_id !== '' ? $state.params.class_id : null );
 	$scope.filters.subject_id = ( $state.params.subject_id !== '' ? $state.params.subject_id : null );
-	$scope.filterClass = ( $state.params.class_id !== '' ? true : false );	
+	$scope.filterClass = ( $state.params.class_id !== '' ? true : false );
 	$scope.alert = {};
 	$scope.loading = true;
-	
+
+	$scope.isTeacher = ( $rootScope.currentUser.user_type == 'TEACHER' ? true : false );
+	if($scope.isTeacher == true){
+		document.getElementById('commTab').style.display = 'none';
+	}
+
 	$scope.gridFilter = {};
 	$scope.gridFilter.filterValue  = '';
-	
-	var rowTemplate = function() 
+
+	var rowTemplate = function()
 	{
 		return '<div class="clickable">' +
 		'  <div ng-if="row.entity.merge">{{row.entity.title}}</div>' +
 		'  <div ng-if="!row.entity.merge" ng-repeat="(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name" class="ui-grid-cell" ng-class="{ \'ui-grid-row-header-cell\': col.isRowHeader }"  ui-grid-cell></div>' +
 		'</div>';
 	}
-	
+
 	$scope.gridOptions = {
 		enableSorting: true,
 		rowTemplate: rowTemplate(),
@@ -36,7 +41,7 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 			{ name: 'Due Date', field: 'due_date2', type: 'date', cellFilter: 'date', enableColumnMenu: false, cellTemplate:'<div class="ui-grid-cell-contents" ng-click="grid.appScope.viewPost(row.entity)">{{row.entity.due_date2}}</div>'},
 			{ name: 'Status', field: 'post_status', width:75, enableColumnMenu: false, cellTemplate:'<div class="ui-grid-cell-contents" ng-click="grid.appScope.viewPost(row.entity)">{{row.entity.post_status}}</div>'},
 			{ name: 'View', field: '', cellClass:'center', width:40, headerCellClass:'center', enableColumnMenu: false, cellTemplate:'<div class="ui-grid-cell-contents" ng-click="grid.appScope.preview(row.entity)"><i class="fa fa-eye"></i></div>'},
-			
+
 		],
 		onRegisterApi: function(gridApi){
 		  $scope.gridApi = gridApi;
@@ -46,63 +51,63 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 		  });
 		}
 	};
-	
-	var initializeController = function () 
-	{		
+
+	var initializeController = function ()
+	{
 		setTimeout(function(){
 			var height = $('.full-height.datagrid').height();
 			$('#grid1').css('height', height);
 			$scope.gridApi.core.handleWindowResize();
 		},100);
-		
+
 		/* get post statuses if not set */
 		if( $rootScope.postStatuses === undefined )
 		{
 			apiService.getBlogPostStatuses({}, function(response){
 				var result = angular.fromJson(response);
-				
+
 				// store these as they do not change often
-				if( result.response == 'success') 
+				if( result.response == 'success')
 				{
 					$scope.postStatuses = result.data;
 					$rootScope.postStatuses = $scope.postStatuses;
-				}		
-				
+				}
+
 			}, apiError);
 		}
 		else
 		{
 			$scope.postStatuses = $rootScope.postStatuses;
 		}
-		
+
 		/* get all the teachers class subjects */
-		getClassSubjects();		
+		getClassSubjects();
 	}
 	$timeout(initializeController,1);
-	
+
 	$scope.$watch('filters.class_id', function(newVal,oldVal){
 		if( newVal == oldVal ) return;
-		
+
 		/* filter the subjects based on selected class */
 		$scope.classSubjects = $scope.allClassSubjects.reduce(function(sum,item){
 			if( item.class_id == newVal ) sum.push(item);
 			return sum;
 		},[]);
 		$timeout(setSearchBoxPosition,10);
-		
+
 	});
 
 	var getClassSubjects = function()
-	{	
+	{
 		var params = $rootScope.currentUser.emp_id;
 		apiService.getTeacherClassSubjects(params, function(response,status){
 			var result = angular.fromJson(response);
-			
+
 			if( result.response == 'success')
-			{	
-				$scope.allClassSubjects = ( result.nodata ? [] : result.data );	
+			{
+				$scope.allClassSubjects = ( result.nodata ? [] : result.data );
 				if( $scope.allClassSubjects.length > 0 )
-				{				
+				{
 					/* build array of unique classes for classes drop down */
 					$scope.classes = $scope.allClassSubjects.reduce(function(sum,item){
 						var classObj = {
@@ -113,7 +118,7 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 						return sum;
 					}, []);
 
-					
+
 					//if( $scope.filters.class_id === null ) $scope.filters.class_id = $scope.classes[0].class_id;
 					if( $scope.filters.subject_id !== null )
 					{
@@ -122,12 +127,12 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 							if( item.class_id == $scope.filters.class_id ) sum.push(item);
 							return sum;
 						},[]);
-		
+
 						/* set selected class subject */
 						var activeClassSubject = $scope.allClassSubjects.filter(function(item){
 							if( item.class_id == $scope.filters.class_id && item.subject_id == $scope.filters.subject_id) return item;
 						})[0];
-						
+
 						$scope.filters.class_subject_id = activeClassSubject.class_subject_id;
 					}
 
@@ -144,12 +149,12 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 				$scope.error = true;
 				$scope.errMsg = result.data;
 			}
-			
+
 		}, apiError);
 
-		
+
 	}
-	
+
 	var containsClassId = function(obj, list) {
 		var i;
 		for (i = 0; i < list.length; i++) {
@@ -160,7 +165,7 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 
 		return false;
 	}
-	
+
 	var getHomework = function(filters)
 	{
 		var params = (filters.post_status_id || 'All') + '/' + (filters.class_subject_id || 'All') + '/' + (filters.class_id || 'All');
@@ -168,10 +173,10 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 		else params += "/0";
 		apiService.getHomeworkPosts(params, function(response,status){
 			var result = angular.fromJson(response);
-			
+
 			if( result.response == 'success')
-			{	
-				$scope.homework = ( result.nodata ? [] : result.data );	
+			{
+				$scope.homework = ( result.nodata ? [] : result.data );
 
 				$scope.homework = $scope.homework.map(function(item){
 					item.assigned_date2 = moment(item.assigned_date).format('MMM Do YYYY, h:mm a');
@@ -188,32 +193,32 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 			$scope.loading = false;
 		}, apiError);
 	}
-	
+
 	$scope.loadFilter = function()
 	{
 		$scope.loading = true;
-		getHomework( angular.copy($scope.filters) );		
+		getHomework( angular.copy($scope.filters) );
 	}
-		
-	var initDataGrid = function(data) 
+
+	var initDataGrid = function(data)
 	{
 		$scope.gridOptions.data = data;
 		$scope.loading = false;
 		$rootScope.loading = false;
-		
+
 	}
-	
-	$scope.filterDataTable = function() 
+
+	$scope.filterDataTable = function()
 	{
 		$scope.gridApi.grid.refresh();
 	};
-	
-	$scope.clearFilterDataTable = function() 
+
+	$scope.clearFilterDataTable = function()
 	{
 		$scope.gridFilter.filterValue = '';
 		$scope.gridApi.grid.refresh();
 	};
-	
+
 	$scope.singleFilter = function( renderableRows )
 	{
 		var matcher = new RegExp($scope.gridFilter.filterValue, 'i');
@@ -230,7 +235,7 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 		});
 		return renderableRows;
 	};
-	
+
 	var setSearchBoxPosition = function()
 	{
 		if( !$rootScope.isSmallScreen )
@@ -239,13 +244,13 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 			$('#resultsTable_filter').css('left',filterFormWidth+55);
 		}
 	}
-	
+
 	var setResizeEvent = function()
 	{
 		 initialLoad = false;
 
 		 $window.addEventListener('resize', function() {
-			
+
 			$rootScope.isSmallScreen = (window.innerWidth < 768 ? true : false );
 			if( $rootScope.isSmallScreen )
 			{
@@ -254,18 +259,18 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 			else
 			{
 				var filterFormWidth = $('.dataFilterForm form').width();
-				$('#resultsTable_filter').css('left',filterFormWidth-30);	
+				$('#resultsTable_filter').css('left',filterFormWidth-30);
 			}
 		}, false);
 	}
-	
-	var apiError = function (response, status) 
+
+	var apiError = function (response, status)
 	{
 		var result = angular.fromJson( response );
 		$scope.error = true;
 		$scope.errMsg = result.data;
 	}
-	
+
 	$scope.preview = function(post)
 	{
 		var data = {
@@ -274,56 +279,56 @@ function($scope, $rootScope, apiService, $timeout, $window, $filter, $state){
 		}
 		$scope.openModal('communications', 'previewPost', 'md', data);
 	}
-	
+
 	$scope.addPost = function()
-	{		
+	{
 		$state.go('communications/add_post', {class_subject_id: $scope.filters.class_subject_id, post_type:'post'});
 	}
-	
+
 	$scope.addHomework = function()
-	{		
+	{
 		$state.go('communications/add_post', {class_subject_id: $scope.filters.class_subject_id, post_type:'homework'});
 	}
-	
+
 	$scope.addEmail = function()
-	{		
+	{
 		$state.go('communications/add_post', { post_type:'communication'});
 	}
-	
-	
+
+
 	$scope.viewPost = function(item)
 	{
 		$state.go('communications/edit_post', {post: item, post_id: item.homework_id, post_type: 'homework'});
 	}
-	
+
 	$scope.$on('refreshPosts', function(event, args) {
 
 		$scope.loading = true;
 		$rootScope.loading = true;
-		
+
 		if( args !== undefined )
 		{
 			$scope.updated = true;
 			$scope.notificationMsg = args.msg;
 		}
 		$scope.refresh();
-		
+
 		// wait a bit, then turn off the alert
 		$timeout(function() { $scope.alert.expired = true;  }, 2000);
-		$timeout(function() { 
+		$timeout(function() {
 			$scope.updated = false;
-			$scope.notificationMsg = ''; 
+			$scope.notificationMsg = '';
 			$scope.alert.expired = false;
 		}, 3000);
 	});
-	
-	$scope.refresh = function () 
+
+	$scope.refresh = function ()
 	{
 		$scope.loading = true;
 		$rootScope.loading = true;
 		getHomework( angular.copy($scope.filters)  );
 	}
-	
+
 	$scope.$on('$destroy', function() {
 		$rootScope.isModal = false;
     });
