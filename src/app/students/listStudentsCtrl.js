d1d37626ab98e96c9fb7865a780885eba957bcae
andy15la <andrew_urban@ymail.com>
2018-06-19 09:26:47 +0300

    bulk print class filter

--- src/app/students/listStudentsCtrl.js
+++ src/app/students/listStudentsCtrl.js
@@ -7,7 +7,7 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
   var initialLoad = true;
   $scope.filters = {};
   $scope.filters.status = 'true';
-  $scope.filters.date = {startDate:null, endDate:null}; 
+  $scope.filters.date = {startDate:null, endDate:null};
 
   var lastQueriedDateRange = null;
   var requery = false;
@@ -25,6 +25,12 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
 
   $scope.isTeacher = ( $rootScope.currentUser.user_type == 'TEACHER' ? true : false);
 
+  $scope.ViewCats = true;
+  $scope.term = {};
+
+
+
+
   var rowTemplate = function()
   {
     return '<div class="clickable" ng-class="{\'alert-danger\': row.entity.balance > 0, \'alert-success\' : row.entity.balance == 0}" ng-click="grid.appScope.viewStudent(row.entity)">' +
@@ -37,11 +43,17 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
     enableSorting: true,
     rowTemplate: rowTemplate(),
     rowHeight:24,
+	headerTemplate: 'app/students/headerTemplate.html',
+	category: [
+             { name: 'main', visible: true, showCatName: false },
+             { name: 'REPORTS CARDS', visible: true, showCatName: true }
+        ],
     columnDefs: [
-      { name: 'Name', field: 'student_name', enableColumnMenu: false, sort: {direction:'asc'}},
-      { name: 'Class', field: 'class_name', enableColumnMenu: false,},
-      { name: 'Admission Number', field: 'admission_number', enableColumnMenu: false,},
-      { name: 'Admission Date', field: 'admission_date', type:'date', cellFilter:'date', enableColumnMenu: false,},
+      { name: 'Name', field: 'student_name', category: 'main', enableColumnMenu: false, sort: {direction:'asc'}},
+      { name: 'Class', field: 'class_name', category: 'main', enableColumnMenu: false,},
+      { name: 'Admission Number', field: 'admission_number', category: 'main', enableColumnMenu: false,},
+      { name: 'Admission Date', field: 'admission_date', category: 'main', type:'date', cellFilter:'date', enableColumnMenu: false,},
+
     ],
     exporterCsvFilename: 'students.csv',
     onRegisterApi: function(gridApi){
@@ -161,6 +173,9 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
 
         getStudents('true',false);
       }
+
+	  $scope.filters.term_name = 'TERM 1';
+
     }
 
     // get terms
@@ -184,6 +199,8 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
     }
 
 
+
+
 		// get fee items
 		apiService.getFeeItems(true, function(response){
 			var result = angular.fromJson(response);
@@ -194,7 +211,7 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
 			}
 
 		}, function(){});
-    
+
     // get transport routes
 		apiService.getTansportRoutes({}, function(response){
 			var result = angular.fromJson(response);
@@ -205,10 +222,10 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
 			}
 
 		}, function(){});
-	
+
     var studentTypes = $rootScope.currentUser.settings['Student Types'];
 		$scope.studentTypes = studentTypes.split(',');
-    
+
     setTimeout(function(){
       var height = $('.full-height.datagrid').height();
       $('#grid1').css('height', height);
@@ -291,7 +308,202 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
     $scope.loading = false;
     $rootScope.loading = false;
 
+	//Get report cards
+
+	//for(var i=0;i<$scope.students.length;i++) {
+	//		console.log($scope.students[i].student_id)
+	//		apiService.getStudentReportCards($scope.students[i].student_id, loadReportCards, apiError);
+	//	}
+
+
+
   }
+
+
+
+
+  var loadReportCards = function(response,status)
+	{
+
+		$scope.loading = false;
+
+		var result = angular.fromJson( response );
+		if( result.response == 'success' )
+		{
+			if( result.nodata )
+			{
+				$scope.reportCards = {};
+				$scope.reportsNotFound = true;
+				$scope.errMsg = "There are currently no report cards entered for this student.";
+			}
+			else
+			{
+
+
+				$scope.rawReportCards = result.data;
+
+				$scope.reportCards = {};
+
+				// get unique terms
+				$scope.reportCards.terms = $scope.rawReportCards.reduce(function(sum,item){
+					if( sum.indexOf(item.term_name) === -1 ) sum.push(item.term_name);
+					return sum;
+				}, []);
+
+
+				// group the reports by class
+				$scope.reportCards.classes = [];
+				var lastClass = '';
+				var lastTerm = '';
+				var reports = {};
+				var i = 0;
+				var student_id = result.data[0].student_id;
+				angular.forEach($scope.rawReportCards, function(item,key){
+
+					if( item.class_name != lastClass )
+					{
+						// changing to new class, store the report
+						if( i > 0 ) $scope.reportCards.classes[(i-1)].reports = reports;
+
+						$scope.reportCards.classes.push(
+							{
+								report_card_id: item.report_card_id,
+								class_name: item.class_name,
+								class_id: item.class_id,
+								class_cat_id: item.class_cat_id,
+								report_card_type: item.report_card_type,
+								teacher_id: item.teacher_id,
+								teacher_name: item.teacher_name,
+								term_id: item.term_id,
+								date: item.date,
+								year: item.year,
+								published: item.published
+
+
+							}
+						);
+
+						reports = {};
+						i++;
+
+					}
+					reports[item.term_name] = {
+						term_id : item.term_id,
+						year: item.year,
+						published : item.published,
+						report_card_id: item.report_card_id,
+						report_card_type: item.report_card_type,
+						class_name: item.class_name,
+						class_id: item.class_id,
+						teacher_id: item.teacher_id,
+						teacher_name: item.teacher_name,
+						date: item.date,
+						data: item.report_data,
+						entity_id: item.entity_id
+					};
+
+					lastClass = item.class_name;
+					lastTerm = item.term_name;
+
+				});
+				$scope.reportCards.classes[(i-1)].reports = reports;
+
+				$scope.studentReports[student_id] = $scope.reportCards.classes[(i-1)].reports;
+
+			}
+
+
+			var cnt = 0;
+
+			angular.forEach($scope.studentReports, function(value, key) {
+			   cnt++;
+			  });
+
+
+			if(cnt === $scope.students.length)
+			{
+
+
+
+				angular.forEach($scope.terms, function(value, key) {
+				var v =   value.term_name;
+					var span = '<input type="submit" class="btn btn-link"  ng-click="grid.appScope.bulkPrint(\'' + v + '\')" value="PRINT" />'
+					var col ='<div class="ui-grid-cell-contents" >' + value.term_name + span + '</div>'
+
+					var click = 'ng-click="getReportCard(item, term.term_name, item.reports[term.term_name])"'
+					var cell = '<span  class="glyphicon glyphicon-file icon-lg"></span>'
+
+					//$scope.gridOptions.columnDefs.push({ name: value.term_name, field: 'none', category: 'REPORTS CARDS', headerCellTemplate: col, cellTemplate: cell,  enableColumnMenu: false,});
+
+
+				  });
+
+
+				$scope.showPrint = true;
+			}
+		}
+		else
+		{
+			$scope.reportsNotFound = true;
+			$scope.errMsg = result.data;
+		}
+	}
+
+
+
+  var BulkData = [];
+  $scope.bulkPrint = function(term_name)
+	{
+		BulkData = [];
+		term_name  =$scope.filters.term_name;
+		angular.forEach($scope.studentReports, function(item,key)
+		{
+
+			
+			var student = $scope.students.find(function (stud) { return stud.student_id === key; });
+			var class_id = $scope.studentReports[student.student_id][term_name].class_id;
+			var class_obj = $scope.classes.find(function (obj) { return obj.class_id === class_id; });
+
+
+				var data =
+				{
+					student : student,
+					report_card_id: $scope.studentReports[student.student_id][term_name].report_card_id,
+					class_name : $scope.studentReports[student.student_id][term_name].class_name,
+					class_id : $scope.studentReports[student.student_id][term_name].class_id,
+					published: $scope.studentReports[student.student_id][term_name].published,
+					term_id: $scope.studentReports[student.student_id][term_name].term_id,
+					entity_id: $scope.studentReports[student.student_id][term_name].entity_id,
+					term_name : term_name,
+					year: $scope.studentReports[student.student_id][term_name].year,
+					report_card_type: $scope.studentReports[student.student_id][term_name].report_card_type,
+					teacher_id: $scope.studentReports[student.student_id][term_name].teacher_id,
+					teacher_name: $scope.studentReports[student.student_id][term_name].teacher_name,
+					date: $scope.studentReports[student.student_id][term_name].date,
+					reportData: $scope.studentReports[student.student_id][term_name].data,
+					adding: false,
+					filters:{
+						term:{
+							term_name:term_name,
+							term_id: $scope.studentReports[student.student_id][term_name].term_id,
+						},
+						class:{
+							class_id: $scope.studentReports[student.student_id][term_name].class_id,
+							class_cat_id: class_obj.class_cat_id
+						}
+					}
+
+				};
+
+				BulkData[student.student_id] = data;
+			});
+
+			$scope.openModal('exams', 'reportCardData', 'sm', angular.fromJson(BulkData));
+
+
+	}
+
+
 
   $scope.filterDataTable = function()
   {
@@ -401,6 +613,8 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
 
   $scope.loadFilter = function()
   {
+	 $scope.studentReports = [];
+
     $scope.loading = true;
     isFiltered = true;
     // make a copy of the current active filters
@@ -424,7 +638,7 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
 
     // store the current status filter
     currentStatus = $scope.activeFilters.status;
-    
+
   }
 
   var filterStudents = function(clearTable)
@@ -444,8 +658,18 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
 
     if( $scope.activeFilters.class_id )
     {
+
       filteredResults = filteredResults.filter(function(item) {
-        if( item.class_id.toString() == $scope.activeFilters.class_id.toString() ) return item;
+        if( item.class_id.toString() == $scope.activeFilters.class_id.toString() )
+		{
+
+			apiService.getStudentReportCards(item.student_id, loadReportCards, apiError);
+			return item;
+
+		}
+
+
+
       });
     }
 
@@ -454,14 +678,14 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
       filteredResults = filteredResults.filter(function(item) {
         if( item.student_type && item.student_type.toString() == $scope.activeFilters.student_type.toString() ) return item;
       });
-    }    
+    }
     if( $scope.activeFilters.course_id )
     {
-      filteredResults = filteredResults.filter(function(item) {         
+      filteredResults = filteredResults.filter(function(item) {
         if( item.enrolled_opt_courses && item.enrolled_opt_courses.indexOf($scope.activeFilters.course_id.toString()) > -1 ) return item;
       });
     }
-    
+
     if( $scope.activeFilters.route_id )
     {
       console.log($scope.activeFilters.route_id);
@@ -469,7 +693,7 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
         if( item.transport_route_id && item.transport_route_id.toString() == $scope.activeFilters.route_id.toString() ) return item;
       });
     }
-    
+
 
     $scope.students = filteredResults;
 
@@ -519,7 +743,7 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
         classes: $scope.classes
       }
       $scope.openModal('students', 'promoteStudents', 'sm', data);
-      
+
     }
     else
     {
@@ -531,10 +755,10 @@ function($scope, $rootScope, apiService, $timeout, $window, $state, $dialogs){
       }
       $scope.openModal('students', 'promoteStudents', 'sm',data);
     }
-    
-    
+
+
   }
-  
+
   $scope.$on('refreshStudents', function(event, args) {
 
     $scope.loading = true;
